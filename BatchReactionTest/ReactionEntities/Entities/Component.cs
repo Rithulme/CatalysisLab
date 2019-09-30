838808da0279174e50b2a59dc516b848367964b0
unknown <tho.bogaerts@gmail.com>
2019-09-30 22:12:16 +0200

    First entities added

---
+++ BatchReactionTest/ReactionEntities/Entities/Component.cs
@@ -0,0 +1,34 @@
+﻿
+namespace ReactionEntities
+{
+    public class Component
+    {
+        private string _name;
+        private string _chemicalComposition;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public string ChemicalComposition
+        {
+            get { return _chemicalComposition; }
+            set { _chemicalComposition = value; }
+        }
+
+        public Component(string name, string composition)
+        {
+            Name = name;
+            ChemicalComposition = composition;
+        }
+
+        public bool Equals(Component comparedComponent)
+        {
+            return string.Equals(Name, comparedComponent.Name);
+        }
+
+
+    }
+}
---
+++ BatchReactionTest/ReactionEntities/Entities/ElementaryReaction.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReactionEntities.Entities
+{
+    class ElementaryReaction
+    {
+        private Component[] _leftHandSide;
+        private Component[] _rightHandSide;
+        private double _preExponentialFactor;
+        private double _activationEnergy;
+
+        public Component[] LeftHandSide
+        {
+            get { return _leftHandSide; }
+            set { _leftHandSide = value; }
+        }
+
+        public Component[] RightHandSide
+        {
+            get { return _rightHandSide; }
+            set { _rightHandSide = value; }
+        }        
+
+        public double PreExponentialFactor
+        {
+            get { return _preExponentialFactor; }
+            set { _preExponentialFactor = value; }
+        }
+
+        public double ActivationEnergy
+        {
+            get { return _activationEnergy; }
+            set { _activationEnergy = value; }
+        }
+
+        public ElementaryReaction(List<Component> leftHandSide, List<Component> rightHandSide, double preExponentialFactor, double activationEnergy)
+        {
+            LeftHandSide = new Component[leftHandSide.Count];
+            RightHandSide = new Component[rightHandSide.Count];
+
+            LeftHandSide = leftHandSide.ToArray();
+            RightHandSide = rightHandSide.ToArray();
+
+            PreExponentialFactor = preExponentialFactor;
+            ActivationEnergy = activationEnergy;
+        }
+
+        public double RateCoefficient(double temperature)
+        {
+            return PreExponentialFactor * Math.Exp(-ActivationEnergy / (PhysicalConstants.GASCONSTANT * temperature));
+        }
+
+    }
+}
