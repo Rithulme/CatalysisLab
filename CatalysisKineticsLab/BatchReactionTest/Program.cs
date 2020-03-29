68bab9b023878b1c29fc7d8f112370a3eabedde4
Rithulme <tho.bogaerts@gmail.com>
2020-03-29 13:52:23 +0200

    Adding example 1

--- CatalysisKineticsLab/BatchReactionTest/Program.cs
+++ CatalysisKineticsLab/BatchReactionTest/Program.cs
@@ -1,15 +1,40 @@
-﻿using System;
+﻿using ProblemTypes;
+using Reaction.Entities;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BatchReactionTest
 {
     class Program
     {
+        List<Component> componentList;
+
         static void Main(string[] args)
         {
+        }
+
+        public List<Component> MakeComponenets()
+        {
+            var componentList = new List<Component>();
+            componentList.Add(new Component("A", "A"));
+            componentList.Add(new Component("B", "B"));
+            componentList.Add(new Component("C", "C"));
+
+            return componentList;
+        }
+
+        public BatchProblemNoDiffusion CreateExampleOne()
+        {
+            // define partial reactions
+            var ListReaction1LHS = new List<Tuple<Component, int>>();
+            var ListReaction1RHS = new List<Tuple<Component, int>>();
+            ListReaction1LHS.Add(new Tuple<Component, int>(componentList[0], 1));
+            ListReaction1RHS.Add(new Tuple<Component, int>(componentList[1], 1));
+
+            var Reaction1 = new ElementaryReaction(ListReaction1LHS, ListReaction1RHS);
+
+            var returnproblem = new BatchProblemNoDiffusion(1000.0,10,);
+
         }
     }
 }
