eefa01abe4b157392776ee9fdac9f610d8a77975
Rithulme <tho.bogaerts@gmail.com>
2020-04-27 13:46:41 +0200

    Added option to load exercise from GUI

--- CatalysisKineticsLab/StudentLabGui/MainWindow.xaml.cs
+++ CatalysisKineticsLab/StudentLabGui/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Exercise;
+using Microsoft.Win32;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using UtilityTools;
 
 namespace StudentLabGui
 {
@@ -20,9 +10,21 @@ namespace StudentLabGui
     /// </summary>
     public partial class MainWindow : Window
     {
+        private BasicExercise loadedExercise;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnBrowse_Click(object sender, RoutedEventArgs e)
+        {
+            var JSONReader = new JSONHandler();
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                loadedExercise = JSONReader.DeSerializeObject<BasicExercise>(openFileDialog.FileName);
+                ExerciseName.Text = loadedExercise.Name;
+            }        
         }
     }
 }
