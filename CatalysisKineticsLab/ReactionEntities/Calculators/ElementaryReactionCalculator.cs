d8d958790a7909091347dcac995efe89ee347c8e
Rithulme <tho.bogaerts@gmail.com>
2020-12-18 22:43:06 +0100

    refactor reactioncalculators

--- CatalysisKineticsLab/ReactionEntities/Calculators/ElementaryReactionCalculator.cs
+++ CatalysisKineticsLab/ReactionEntities/Calculators/ElementaryReactionCalculator.cs
@@ -14,62 +14,47 @@ namespace Reaction.Calculators
         public Dictionary<Component, double> UpdateConcentration(ElementaryReaction reaction ,Dictionary<Component, double> currentConcentration, double currentTemperature, double timestep)
         {
             var returnDictionary = new Dictionary<Component, double>(new Component.EqualityComparer());
+
+            //calculate rate
+            double prodConcentration = 1.0;
             foreach (var reactionElement in reaction.LeftHandSide)
             {
-                double prodConcentration = 1.0;
-                foreach (var reactionElementBis in reaction.LeftHandSide)
-                {
-                    prodConcentration = prodConcentration * Math.Pow(currentConcentration[reactionElementBis.ReactionComponent], reactionElementBis.Power);
-                }
-                double concentrationChange = -reaction.ForwardRateCoefficient(currentTemperature) * prodConcentration * timestep;
-
-                prodConcentration = 1.0;
-                foreach (var reactionElementBis in reaction.RightHandSide)
-                {
-                    prodConcentration = prodConcentration * Math.Pow(currentConcentration[reactionElementBis.ReactionComponent], reactionElementBis.Power);
-                }
-                concentrationChange = concentrationChange + reaction.BackwardRateCoefficient(currentTemperature) * prodConcentration * timestep;
-
-                if (!returnDictionary.ContainsKey(reactionElement.ReactionComponent))
-                {
-                    returnDictionary.Add(reactionElement.ReactionComponent, concentrationChange);
-                }
-                else
-                {
-                    var tempconc = returnDictionary[reactionElement.ReactionComponent];
-                    returnDictionary.Remove(reactionElement.ReactionComponent);
-                    returnDictionary.Add(reactionElement.ReactionComponent, tempconc + concentrationChange);
-                }
+                prodConcentration = prodConcentration * Math.Pow(currentConcentration[reactionElement.ReactionComponent], reactionElement.Power);
             }
+            var forwardRate = reaction.ForwardRateCoefficient(currentTemperature) * prodConcentration * timestep;
 
+            prodConcentration = 1.0;
             foreach (var reactionElement in reaction.RightHandSide)
             {
-                double prodConcentration = 1.0;
-                foreach (var reactionElementBis in reaction.LeftHandSide)
-                {
-                    prodConcentration = prodConcentration * Math.Pow(currentConcentration[reactionElementBis.ReactionComponent], reactionElementBis.Power);
-                }
-                double concentrationChange = reaction.ForwardRateCoefficient(currentTemperature) * prodConcentration * timestep;
+                prodConcentration = prodConcentration * Math.Pow(currentConcentration[reactionElement.ReactionComponent], reactionElement.Power);
+            }
+            var backwardRate = reaction.BackwardRateCoefficient(currentTemperature) * prodConcentration * timestep;
 
-                prodConcentration = 1.0;
-                foreach (var reactionElementBis in reaction.RightHandSide)
+            //calculate concentration changes associated to rates
+            foreach (var element in reaction.LeftHandSide)
+            {
+                if (!returnDictionary.ContainsKey(element.ReactionComponent))
                 {
-                    prodConcentration = prodConcentration * Math.Pow(currentConcentration[reactionElementBis.ReactionComponent], reactionElementBis.Power);
-                }
-                concentrationChange = concentrationChange - reaction.BackwardRateCoefficient(currentTemperature) * prodConcentration * timestep;
-
-                if (!returnDictionary.ContainsKey(reactionElement.ReactionComponent))
-                {
-                    returnDictionary.Add(reactionElement.ReactionComponent, concentrationChange);
+                    returnDictionary.Add(element.ReactionComponent.Copy(), (backwardRate - forwardRate) * element.Power);
                 }
                 else
                 {
-                    var tempconc = returnDictionary[reactionElement.ReactionComponent];
-                    returnDictionary.Remove(reactionElement.ReactionComponent);
-                    returnDictionary.Add(reactionElement.ReactionComponent, tempconc + concentrationChange);
+                    returnDictionary[element.ReactionComponent] += (backwardRate - forwardRate) * element.Power;
                 }
             }
 
+            foreach (var element in reaction.RightHandSide)
+            {
+                if (!returnDictionary.ContainsKey(element.ReactionComponent))
+                {
+                    returnDictionary.Add(element.ReactionComponent.Copy(), (-backwardRate + forwardRate) * element.Power);
+                }
+                else
+                {
+                    returnDictionary[element.ReactionComponent] += (-backwardRate + forwardRate) * element.Power;
+                }
+            }
+                                   
             return returnDictionary;
         }
 
--- CatalysisKineticsLab/ReactionEntities/Entities/ReactionElement.cs
+++ CatalysisKineticsLab/ReactionEntities/Entities/ReactionElement.cs
@@ -8,25 +8,14 @@ namespace Reaction.Entities
 {
     public class ReactionElement
     {
-        private Component _reactionComponent;
-        private int _power;
+        public Component ReactionComponent { get; set; }
 
-        public Component ReactionComponent
-        {
-            get { return _reactionComponent; }
-            set { _reactionComponent = value; }
-        }
-
-        public int Power
-        {
-            get { return _power; }
-            set { _power = value; }
-        }
+        public int Power { get; set; }
 
         public ReactionElement(Component component, int power)
         {
-            _power = power;
-            _reactionComponent = component;
+            Power = power;
+            ReactionComponent = component;
         }
 
         public ReactionElement()
